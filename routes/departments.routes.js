fbc9346594be7b8bdbc585677a4c4df89e925bd0
unknown <kowalczyk.przemek@wp.pl>
2021-04-24 01:21:51 +0200

    Add respond update and delete files after useing those methods

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -53,7 +53,8 @@ router.put('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name } });
-      res.json({ message: 'OK' });
+      const updatedDep = await Department.findById(req.params.id);
+      res.json({ message: 'OK', updated: updatedDep });
     }
     else res.status(404).json({ message: 'Not found...' })
   }
@@ -66,8 +67,8 @@ router.delete('/departments/:id', async (req, res) => {
   try {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
-      await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      const forDelete = await Department.findByIdAndDelete({ _id: req.params.id });
+      res.json({ message: 'OK', deleted: forDelete });
     }
     else res.status(404).json({ message: 'Not found...' })
   }
