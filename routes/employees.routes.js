381da49859562ad875f969190cd81d493dfe8593
unknown <kowalczyk.przemek@wp.pl>
2021-04-24 21:40:49 +0200

    Add populate() to employees.routes.js endpoints

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());  
+    res.json(await Employee.find().populate('department'));  
   } 
   catch(err) {
     res.status(500).json({ message: err });
@@ -15,7 +15,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const emp = await Employee.findOne().skip(rand);
+    const emp = await Employee.findOne().skip(rand).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found...' }); 
     else res.json(emp);     
   } 
@@ -26,7 +26,7 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const emp = await Employee.findById(req.params.id);
+    const emp = await Employee.findById(req.params.id).populate('department');
     if(!emp) res.status(404).json({ message: 'Not found...' });
     else res.json(emp);
   } 
@@ -52,7 +52,7 @@ router.put('/employees/:id', async (req, res) => {
   try {
     const emp = await(Employee.findById(req.params.id));
     if(emp) {
-      await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department } });
+      await Employee.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department } }).populate('department');
       res.json({ message: 'OK' });
     } 
     else res.status(404).json({ message: 'Not found...' })    
@@ -66,8 +66,8 @@ router.delete('/employees/:id', async (req, res) => {
   try {
     const emp = await(Employee.findById(req.params.id)); 
     if(emp) {
-      await Employee.deleteOne({ _id: req.params.id});
-      res.json({ message: 'OK' });
+      const forDelete = await Employee.findByIdAndDelete({ _id: req.params.id});
+      res.json({ message: 'OK', deleted: forDelete }).populate('department');
     } 
     else res.status(404).json({ message: 'Not found...' });
   } 
