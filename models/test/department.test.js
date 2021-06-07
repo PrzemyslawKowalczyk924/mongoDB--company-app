7e36791fc40382a9f77c3634a970fa0428de0d21
unknown <kowalczyk.przemek@wp.pl>
2021-06-07 23:01:24 +0200

    Add department.test.js

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose');
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
 
-const departmentSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+describe('Department', () => {
+
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); // create new Department, but don't set `name` attr value
+
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    });
+  });
+
 });
-
-module.exports = mongoose.model('Department', departmentSchema);
