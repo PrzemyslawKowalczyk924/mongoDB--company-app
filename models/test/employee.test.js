1b6e7380f4e760aa4c08732ee557f261441b03b1
unknown <kowalczyk.przemek@wp.pl>
2021-06-09 22:52:56 +0200

    Add more tests to employee

--- models/test/employee.test.js
+++ models/test/employee.test.js
@@ -20,13 +20,31 @@ describe('Employee', () => {
   });
 
   it('should throw an error if added arg is not a string', () => {
-    const [firstName, lastName, department] = ['John', {}, 123]
+    const [string, func, array, object] = ['hello world!', function() {}, [], {}]
+    const example1 = new Employee({firstName: string, lastName: array, department: string});
+    const example2 = new Employee({firstName: object, lastName: string, department: string});
+    const example3 = new Employee({firstName: string, lastName: array, department: func});
+    const example4 = new Employee({firstName: object, lastName: func, department: array});
+    const example5 = new Employee({firstName: func, lastName: array, department: object});
+
+    const cases = [example1, example2, example3, example4, example5]
+
+    for(let example of cases) {
+      example.validate(err => {
+        expect(err.errors).to.exist;
+      });
+    }
+    
+  });
+
+  it('should not throw an error if all args are okay', () => {
+    const [firstName, lastName, department] = ['John', 'Doe', 'IT']
     const example = new Employee({firstName: firstName, lastName: lastName, department: department});
 
     example.validate(err => {
-      expect(err.errors).to.exist;
+      expect(err).to.not.exist;
     });
-    
+  
   });
 
 });
